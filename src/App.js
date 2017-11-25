1080cfbaac269194df91589fa61ba7b9a058eca6
autsakornta <autsakorn.ta@carmana.com>
2017-11-25 15:59:03 +0700

    Fibo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton'
 
 class App extends Component {
   constructor(props){
@@ -16,18 +18,39 @@ class App extends Component {
     this.setState({ data, value:"" });
   }
   renderSubject = () => {
-    var { data } = this.state;
-    var listData = [];
-    data.map((item, i) => {
-      listData.push(<div key={i}>{item}</div>);
-    });
-    return listData;
+    var { value } = this.state;
+    return <div>{this.fibo2(value)}</div>;
   }
+
+
+  fibo = (number) => {
+    if(number<3){
+      return 1;
+    }else {
+      return this.fibo(number-1) + this.fibo(number-2);
+    }
+  }
+
+  fibo2 = (number) => {
+    var a = 1, b = 1, y = 0;
+    if( number < 3) {
+      return 1;
+    }
+    for(var i = 3; i <= number; i++){
+      y = a + b;
+      a = b;
+      b = y;
+    }
+    return y;
+  }
+
   render() {
     return (
       <div className="App">
-        <input value={this.state.value} onChange={(e)=>this.onSubject(e.target.value)} />
-        <button onClick={()=>this.addData()}>Add</button>
+        <TextField hintText="Enter number" 
+          value={this.state.value} 
+          onChange={(e)=>this.onSubject(e.target.value)} />
+        <FlatButton label="Add" primary={true} onClick={()=>this.addData()} />
         {this.renderSubject()}
       </div>
     );
